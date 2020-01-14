78e0ecea116075145a8a11655118a17e7f3b61e3
Pedro Schneider <pedro.schneider@cwi.com.br>
2020-01-14 20:05:26 -0300

    Commit Inicial

---
+++ cypress/integration/exemplo_spec.js
@@ -0,0 +1,9 @@
+context('Exemplo', () => {
+  beforeEach(() => {
+    cy.visit('https://www.google.com')
+  })
+
+  it('Acessando o Google', function () {
+    // Aqui vai o código de teste
+  })
+})
